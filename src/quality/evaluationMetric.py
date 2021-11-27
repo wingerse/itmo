d4dfc50bc2400958b5f865f610ee231d7a3f0374
Yaaseen Edoo <muhammad@wavelet.net>
2021-11-27 14:57:08 +0800

    luminance conversion in logPSNR evaluation metric

--- src/quality/evaluationMetric.py
+++ src/quality/evaluationMetric.py
@@ -3,11 +3,24 @@ from math import log10
 import numpy as np
 
 
-def logPSNR(testimage, referenceimage):
+def luminanceConversion(R, G, B):
+    luminance = 0.27*R + 0.67*G + 0.06*B
+    return luminance
+
+
+# def findluminance(testimagetriplets, referenceimagetriplets):
+#     testimage = luminanceConversion(testimagetriplets[0], testimagetriplets[1], testimagetriplets[2])
+#     referenceimage = luminanceConversion(referenceimagetriplets[0], referenceimagetriplets[1], referenceimagetriplets[2])
+#     return testimage ,referenceimage
+
+
+def logPSNR(testimagetriplets, referenceimagetriplets):
+    testimage = luminanceConversion(testimagetriplets[0], testimagetriplets[1], testimagetriplets[2])
+    referenceimage = luminanceConversion(referenceimagetriplets[0], referenceimagetriplets[1], referenceimagetriplets[2])
     Lmin =0
     testimageluminance = max(testimage,Lmin)
     referenceimageluminance = max(referenceimage, Lmin)
-    mse = np.mean(( testimageluminance- referenceimageluminance) ** 2)
+    mse = np.mean(( log10(testimageluminance)- log10(referenceimageluminance)) ** 2)
     if (mse == 0):  # means thatno noise present, logPSNR serves no importance here
         return 100
     Lmax = 1000     # as most HDR displays will not exceed this value.(according to Kai Linn's thesis)
