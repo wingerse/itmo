c38166aa8058188c0c077d6774eecffe45419298
Yaaseen Edoo <muhammad@wavelet.net>
2021-12-04 18:24:39 +0800

    complete logPSNR and tested on two  hdr images

--- src/quality/evaluationMetric.py
+++ src/quality/evaluationMetric.py
@@ -42,17 +42,36 @@
 
 from math import log10
 import imageio
+import os
 import cv2
 import numpy as np
 
 from fyp.src import util
 
-def logPSNR(testImage, referenceImage):
+def luminanceConversion(R, G, B):
+    luminance = format(0.27*R + 0.67*G + 0.06*B, '.12g')
+    return luminance
+
+def logPSNR(testImageArray, referenceImageArray):
     Lmin =0
     # print(referenceImage)
-    testImageLuminance = max(testImage,Lmin)
-    referenceImageLuminance = max(referenceImage, Lmin)
-    mse = np.mean((log10(testImageLuminance)- log10(referenceImageLuminance)) ** 2)
+
+    testImageLuminance = np.max(testImageArray,Lmin)
+    # print(testImageLuminance, "bef")
+    for i in range(len(testImageLuminance)):
+        logVal=log10(testImageLuminance[i])
+        testImageLuminance[i] = logVal
+    # print(testImageLuminance, "aft")
+
+    referenceImageLuminance = np.max(referenceImageArray, Lmin)
+    # print(referenceImageLuminance, "befref")
+
+    for i in range(len(referenceImageLuminance)):
+        logVal=log10(referenceImageLuminance[i])
+        referenceImageLuminance[i] = logVal
+    # print(referenceImageLuminance, "aftref")
+    # print(referenceImageLuminance, "referenceluminance")
+    mse = np.mean((testImageLuminance -referenceImageLuminance) ** 2)
     if (mse == 0):  # means that no noise present, logPSNR serves no importance here
         return 100
     Lmax = 10000     # as most HDR displays will not exceed this value.(according to Kai Linn's thesis)
@@ -63,16 +82,42 @@ def main():
     # dir_path = os.path.dirname(os.path.realpath(__file__))
     # Loading images (original image and compressed image)
     # original = cv2.imread(os.path.join(dir_path, 'original_image.png'))
+    # print()
     # contrast = cv2.imread(os.path.join(dir_path, 'compressed_image.png'), 1)
 
-    testImage = util.load_hdr('./generated_hdr_b_0_0.hdr')
-    print(testImage)
-    referenceImage = util.load_hdr('./gt_hdr_b_0_0.hdr')
 
+    testImage = util.load_hdr_image('./generated_hdr_b_0_0.hdr')
+    # print(testImage.shape[0])
+    testImageArray = np.zeros([testImage.shape[0], testImage.shape[1]])
+    # print('array',testImageArray)
+    for h in range(len(testImage)):
+        for w in range(len(testImage[h])):
+            # print(testImage[h][w])
+            # print(testImage[h][w])
+            testImageArray[h][w] = luminanceConversion(testImage[h][w][0], testImage[h][w][1], testImage[h][w][2])
+            # print(array[h][w])
+
+        # print(testImage[h])
+    # for h in testImage:
+    #     for w in h:
+    #         array[h][w] = luminanceConversion(testImage[h][w][0],testImage[h][w][1], testImage[h][w][2])
+    # print(testImageArray)
+
+    # print(np.mean(testImageArray))
+    referenceImage = util.load_hdr_image('./gt_hdr_b_0_0.hdr')
+    referenceImageArray = np.zeros([referenceImage.shape[0], referenceImage.shape[1]])
+    # print('array', referenceImageArray)
+    for h in range(len(referenceImage)):
+        for w in range(len(referenceImage[h])):
+            # print(testImage[h][w])
+            # print(testImage[h][w])
+            referenceImageArray[h][w] = luminanceConversion(referenceImage[h][w][0], referenceImage[h][w][1], referenceImage[h][w][2])
+            # print(array[h][w]
+    # print(referenceImageArray)
 
     # Value expected: 29.73dB
-    print("-- First Test --")
-    print(f"PSNR value is {logPSNR(testImage, referenceImage)} dB")
+    # print("-- First Test --")
+    print(f"log PSNR value is {logPSNR(testImageArray, referenceImageArray)} dB")
 
 
     # # Value expected: 31.53dB (Wikipedia Example)
