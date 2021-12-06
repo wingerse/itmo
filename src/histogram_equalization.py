8839ac0d4da759ce7972b42b45b6c04da18dd200
Yaaseen Edoo <muhammad@wavelet.net>
2021-12-06 17:11:00 +0800

    testing histogram equalization

--- src/histogram_equalization.py
+++ src/histogram_equalization.py
@@ -1,75 +1,75 @@
 # Code taken from https://github.com/samsudinng/cv_histogram_equalization/tree/master/demo_notebook and is bound to changes later
 
 
-import numpy as np
-from PIL import Image
-import matplotlib.pyplot as plt
-
-
-img_filename = 'Unequalized_Hawkes_Bay_NZ.jpg'
-save_filename = 'Equalized_Hawkes_Bay_NZ.jpg'
-
-#load file as pillow Image
-img = Image.open(img_filename)
-
-# convert to grayscale
-imgray = img.convert(mode='L')
-
-#convert to NumPy array
-img_array = np.asarray(imgray)
-
-#flatten image array and calculate histogram via binning
-histogram_array = np.bincount(img_array.flatten(), minlength=256)
-
-#normalize
-num_pixels = np.sum(histogram_array)
-histogram_array = histogram_array/num_pixels
-
-#cumulative histogram
-chistogram_array = np.cumsum(histogram_array)
-transform_map = np.floor(255 * chistogram_array).astype(np.uint8)
-
-# flatten image array into 1D list
-img_list = list(img_array.flatten())
-
-# transform pixel values to equalize
-eq_img_list = [transform_map[p] for p in img_list]
-
-# reshape and write back into img_array
-eq_img_array = np.reshape(np.asarray(eq_img_list), img_array.shape)
-
-
-# Let's plot the histograms
-
-#histogram and cumulative histogram of original image has been calculated above
-ori_cdf = chistogram_array
-ori_pdf = histogram_array
-
-#calculate histogram and cumulative histogram of equalized image
-eq_histogram_array = np.bincount(eq_img_array.flatten(), minlength=256)
-num_pixels = np.sum(eq_histogram_array)
-eq_pdf = eq_histogram_array/num_pixels
-eq_cdf = np.cumsum(eq_pdf)
-
-#plot
-plt.figure()
-plt.plot(ori_pdf)
-plt.plot(eq_pdf)
-plt.xlabel('Pixel intensity')
-plt.ylabel('Distribution')
-plt.legend(['Original','Equalized'])
-plt.figure()
-plt.plot(ori_cdf)
-plt.plot(eq_cdf)
-plt.xlabel('Pixel intensity')
-plt.ylabel('Distribution')
-plt.legend(['Original','Equalized'])
-
-
-eq_img = Image.fromarray(eq_img_array, mode='L')
-eq_img.save(save_filename)
-
-
+# import numpy as np
+# from PIL import Image
+# import matplotlib.pyplot as plt
+#
+#
+# img_filename = 'Unequalized_Hawkes_Bay_NZ.jpg'
+# save_filename = 'Equalized_Hawkes_Bay_NZ.jpg'
+#
+# #load file as pillow Image
+# img = Image.open(img_filename)
+#
+# # convert to grayscale
+# imgray = img.convert(mode='L')
+#
+# #convert to NumPy array
+# img_array = np.asarray(imgray)
+#
+# #flatten image array and calculate histogram via binning
+# histogram_array = np.bincount(img_array.flatten(), minlength=256)
+#
+# #normalize
+# num_pixels = np.sum(histogram_array)
+# histogram_array = histogram_array/num_pixels
+#
+# #cumulative histogram
+# chistogram_array = np.cumsum(histogram_array)
+# transform_map = np.floor(255 * chistogram_array).astype(np.uint8)
+#
+# # flatten image array into 1D list
+# img_list = list(img_array.flatten())
+#
+# # transform pixel values to equalize
+# eq_img_list = [transform_map[p] for p in img_list]
+#
+# # reshape and write back into img_array
+# eq_img_array = np.reshape(np.asarray(eq_img_list), img_array.shape)
+#
+#
+# # Let's plot the histograms
+#
+# #histogram and cumulative histogram of original image has been calculated above
+# ori_cdf = chistogram_array
+# ori_pdf = histogram_array
+#
+# #calculate histogram and cumulative histogram of equalized image
+# eq_histogram_array = np.bincount(eq_img_array.flatten(), minlength=256)
+# num_pixels = np.sum(eq_histogram_array)
+# eq_pdf = eq_histogram_array/num_pixels
+# eq_cdf = np.cumsum(eq_pdf)
+#
+# #plot
+# plt.figure()
+# plt.plot(ori_pdf)
+# plt.plot(eq_pdf)
+# plt.xlabel('Pixel intensity')
+# plt.ylabel('Distribution')
+# plt.legend(['Original','Equalized'])
+# plt.figure()
+# plt.plot(ori_cdf)
+# plt.plot(eq_cdf)
+# plt.xlabel('Pixel intensity')
+# plt.ylabel('Distribution')
+# plt.legend(['Original','Equalized'])
+#
+#
+# eq_img = Image.fromarray(eq_img_array, mode='L')
+# eq_img.save(save_filename)
+#
+#
 
 import cv2 as cv
 import numpy as np
@@ -77,7 +77,6 @@ from matplotlib import pyplot as plt
 
 def histogram_equalization(img):
     img = cv.imread(img)
-
     img_yuv = cv.cvtColor(img, cv.COLOR_BGR2YUV)
 
     # equalize the histogram of the Y channel
@@ -108,4 +107,4 @@ def show_image_comparison(img, img_output):
     # cv.imshow("equalizeHist", np.hstack((img, img_output)))
     return
 
-histogram_equalization('ldr_test.png')
+histogram_equalization('../images/ldr_test3.jpg')
