b42aa6952e41b3e75d1a96bb84b214f7e6aa837d
wingerse <aiman012@live.com>
2021-12-18 01:42:22 +0800

    evaluation with one checkpoint

--- src/evaluate_fhdr.py
+++ src/evaluate_fhdr.py
@@ -2,29 +2,5 @@ from itmo.fhdr.test import test
 from matplotlib import pyplot as plt
 
 if __name__ == "__main__":
-    x = []
-    y_psnr = []
-    y_ssim = []
-    for i in range(1, 37+1):
-        psnr, ssim = test(f"itmo/fhdr/checkpoints/epoch_{i}.ckpt", "../datasets/testing_data", "test_output/fhdr")
-        x.append(i)
-        y_psnr.append(psnr)
-        y_ssim.append(ssim)
-    
-    print(x, y_psnr, y_ssim)
-
-    plt.figure()
-    plt.plot(x, y_psnr)
-    plt.xticks(x)
-    plt.ylabel("PSNR")
-    plt.xlabel("Epochs")
-    plt.savefig("psnr.png")
-
-    plt.figure()
-    plt.plot(x, y_ssim)
-    plt.xticks(x)
-    plt.ylabel("SSIM")
-    plt.xlabel("Epochs")
-    plt.savefig("ssim.png")
-
-    plt.show()
+    psnr, ssim = test(f"itmo/fhdr/checkpoints/FHDR-iter-2.ckpt", "../datasets/aug_dataset_test", "test_output/fhdr")
+    print(psnr, ssim)
